99d715be2609b76f19a32ba32e185def9b7c4a6e
Karthik Sunil <ksnl@caen-vnc-mi12.engin.umich.edu>
2024-10-30 21:41:54 -0400

    Added comments for readability

--- verilog/dc_top.sv
+++ verilog/dc_top.sv
@@ -8,6 +8,7 @@ module dc_top #(
     input logic reset,
     input logic [DATA_WIDTH-1:0] x0,
     input logic [DATA_WIDTH-1:0] x1,
+    
     output logic [DATA_WIDTH-1:0] y0,
     output logic [DATA_WIDTH-1:0] y1,
     output logic dc_chain_out_valid
@@ -24,21 +25,22 @@ module dc_top #(
     generate
         for(i=0; i<NUM_COMMUTATORS; i++) begin
             delay_commutator #(
-                .DELAY(N >> (i+2)),
+                .DELAY(N >> (i+2)),                     // Delay = N/4, N/8, N/16, ... 1
                 .DATA_WIDTH(DATA_WIDTH)
             ) dc_inst (
                 .clk(clk),
                 .reset(reset),
-                .enable(i == 0 ? 1: dc_out_valid[i-1]),
-                .x0(i == 0 ? x0 : x0_dc[i-1]),
-                .x1(i == 0 ? x1 : x1_dc[i-1]),
-                .y0(x0_dc[i]),
+                .enable(i == 0 ? 1: dc_out_valid[i-1]),  // enable signal from the previous commutator out valid, but always 1 for the first commutator
+                .x0(i == 0 ? x0 : x0_dc[i-1]),           // input from previous commutator output, but always x0 for the first commutator
+                .x1(i == 0 ? x1 : x1_dc[i-1]),           // input from previous commutator output, but always x1 for the first commutator
+                .y0(x0_dc[i]),                           // Commutator output wired to input of next stage
                 .y1(x1_dc[i]),
                 .commutator_out_valid(dc_out_valid[i])
             );
         end
     endgenerate
 
+// Sample outputs from last commutator stages
 
 assign y0 = x0_dc[NUM_COMMUTATORS-1];
 assign y1 = x1_dc[NUM_COMMUTATORS-1];
