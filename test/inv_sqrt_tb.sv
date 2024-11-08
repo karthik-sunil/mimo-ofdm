bf0b8e0d4d1d10d7f41697f10d90b450dc7f710e
Karthik Sunil <ksnl@caen-vnc-mi07.engin.umich.edu>
2024-11-08 16:30:18 -0500

    Added file testing for verification. Confirmed  by comparing against python output

--- test/inv_sqrt_tb.sv
+++ test/inv_sqrt_tb.sv
@@ -3,6 +3,8 @@ module inv_sqrt_tb();
 parameter CLOCK_PERIOD = 10;
 parameter I_DATA = 32;
 
+integer f_out;
+
 logic clk;
 logic reset;
 logic enable;
@@ -19,6 +21,30 @@ always begin
     #(CLOCK_PERIOD/2);
 end
 
+task file_test; 
+        integer f1,r1; 
+        logic [255:0] line_f1; 
+
+        // Open the file 
+        f1 = $fopen("dat/inv_sqrt_in.csv","r");
+
+        if (f1 == 0) begin
+            $display("File is missing! Exiting...");
+            $finish; 
+        end
+        while(!$feof(f1)) begin
+            r1 = $fgets(line_f1,f1);
+            if (r1 >0) begin
+                $sscanf(line_f1,"%h", idata);
+                $display("idata = %h", idata);
+                @(negedge clk);
+                end
+            
+            end
+                
+        $fclose(f1);
+endtask
+
 inv_sqrt #(
     .I_DATA(I_DATA)
 ) dut (
@@ -31,18 +57,9 @@ inv_sqrt #(
 );
 
 always @(negedge clk) begin
-    // $display("idata = %h, i = %h, y_1_intermediate = %h, y_1_intermediate_valid = %b, i_delay_9 = %h, i_delay_9_valid = %b, odata = %h, out_valid = %b", 
-    //             idata, 
-    //             dut.i_ff,
-    //           dut.y_1_intermediate,
-    //           dut.y_1_intermediate_valid,
-    //             dut.i_delay_9,
-    //             dut.i_delay_9_valid,
-    //             odata,
-    //             out_valid
-    //           );
+    // $display("idata = %h, odata = %h, out_valid = %b", idata, odata, out_valid);
+    $fdisplay(f_out, "%h, %h, %b", idata, odata, out_valid);
 
-    $display("idata = %h, odata = %h, out_valid = %b", idata, odata, out_valid);
 end
 
 initial begin
@@ -55,14 +72,18 @@ initial begin
     reset = 0;
     enable = 1;
     // Need to test for more inputs
-    idata = 32'h40000000;
-    @(negedge clk);
-    idata = 32'h3E200000;
-    @(negedge clk);
-    idata = 32'h40B66666;
-    @(negedge clk);
-    idata = 32'h40A00000;
+    // idata = 32'h40000000;
+    // @(negedge clk);
+    // idata = 32'h3E200000;
+    // @(negedge clk);
+    // idata = 32'h40B66666;
+    // @(negedge clk);
+    // idata = 32'h40A00000;
+
+    f_out = $fopen("out/inv_sqrt_out.csv","w");
+    file_test;
     repeat(20) @(negedge clk);
+    $fclose(f_out);
     // @(negedge clk);
     $finish;
 
