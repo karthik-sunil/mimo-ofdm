f83b2c1690bf051a72d79d97524ab9208e576d5c
Hithesh Prabhakar Reddy <hitheshp@caen-vnc-mi08.engin.umich.edu>
2024-11-16 17:10:58 -0500

    Butterfly twiddle mult for DIF FFT fixed and tested

--- test/butterfly_tb.sv
+++ test/butterfly_tb.sv
@@ -6,8 +6,8 @@ logic clk;
 logic reset;
 logic enable;
 
-complex_t A;
-complex_t B;
+complex_product_t A;
+complex_product_t B;
 
 complex_product_t X;
 complex_product_t Y;
@@ -35,6 +35,9 @@ always begin
     #(CLOCK_PERIOD/2);
 end
 
+int A_r_values[4] = '{4, 6, 4, 4};
+int B_r_values[4] = '{8, 10, 4, 4};
+
 initial begin
     clk = 0;
     reset = 1;
@@ -43,15 +46,24 @@ initial begin
     reset = 0;
     enable = 1;
 
-    A.r = 2;
-    A.i = 3;
-    B.r = 1;
-    B.i = -1;
+    // A.r = 2;
+    // A.i = 3;
+    // B.r = 1;
+    // B.i = -1;
 
-    for(int i=0; i<5; i=i+1) begin
+    // for(int i=0; i<5; i=i+1) begin
+    //     @(negedge clk);
+    //     $display("A.r=%h, A.i=%h, B.r=%h, B.i=%h, X.r=%h, X.i=%h, Y.r=%h, Y.i=%h, out_valid=%h", A.r, A.i, B.r, B.i, X.r, X.i, Y.r, Y.i, out_valid);
+    // end
+
+    for (int i = 0; i < 4; i++) begin
+        A.r = A_r_values[i];
+        A.i = 0;
+        B.r = B_r_values[i];
+        B.i = 0;
         @(negedge clk);
-        $display("A.r=%h, A.i=%h, B.r=%h, B.i=%h, X.r=%h, X.i=%h, Y.r=%h, Y.i=%h, out_valid=%h", A.r, A.i, B.r, B.i, X.r, X.i, Y.r, Y.i, out_valid);
-    end
+        $display("A.r=%d, A.i=%d, B.r=%d, B.i=%d, X.r=%d, X.i=%d, Y.r=%d, Y.i=%d, out_valid=%d", A.r, A.i, B.r, B.i, X.r, X.i, Y.r, Y.i, out_valid);
+    end    
 
     #100;
     $finish;
--- verilog/butterfly.sv
+++ verilog/butterfly.sv
@@ -15,20 +15,25 @@ module butterfly #(
 );
 
     complex_product_t X_comb, Y_comb;
-    complex_product_t B_W; 
+    complex_product_t Y_Wout; 
 
     always_comb begin
         // Complex multiplication B * W
-        B_W.r = B.r * W_R - B.i * W_I;
-        B_W.i = B.r * W_I + B.i * W_R;
+        // B_W.r = B.r * W_R - B.i * W_I;
+        // B_W.i = B.r * W_I + B.i * W_R;
 
-        // Complex addition A + B * W
-        X_comb.r = A.r + B_W.r;
-        X_comb.i = A.i + B_W.i;
+        // Complex addition A + B
+        X_comb.r = A.r + B.r;
+        X_comb.i = A.i + B.i;
 
-        // Complex subtraction A - B * W
-        Y_comb.r = A.r - B_W.r;
-        Y_comb.i = A.i - B_W.i;
+        // Complex subtraction A - B
+        Y_comb.r = A.r - B.r;
+        Y_comb.i = A.i - B.i;
+
+        // Twiddle factor mult after the stage - DIF FFT
+        Y_Wout.r = Y_comb.r * W_R - Y_comb.i * W_I;
+        Y_Wout.i = Y_comb.r * W_I + Y_comb.i * W_R;
+
     end
 
    always_ff @(posedge clk) begin
@@ -38,7 +43,7 @@ module butterfly #(
             out_valid <= 0;
         end else begin
             X <= X_comb;
-            Y <= Y_comb;
+            Y <= Y_Wout;
             out_valid <= 1'b1;
         end
    end
