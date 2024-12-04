68c6ffd4e4d4eadd084c805bc7fa2d0fbd6a3821
Hithesh Prabhakar Reddy <hitheshp@caen-vnc-mi18.engin.umich.edu>
2024-12-03 21:36:11 -0500

    Customized the constraints script for optimal clock period

--- scripts/constraints.tcl
+++ scripts/constraints.tcl
@@ -1,9 +1,10 @@
-set_max_transition 0.150 ${DESIGN_NAME}
+# set the maximum signal transition time to 0.1 ns
+set_max_transition 0.100 ${DESIGN_NAME}
 set_input_transition 0.080 [all_inputs]
 set_max_transition 0.080 [all_outputs]
 
-set clock_period 26
-set clock_uncertainty [expr $clock_period * 0.10]
+set clock_period 14
+set clock_uncertainty [expr $clock_period * 0.025]
 set clock_transition 0.080
 set clock_latency 0.1
 
@@ -12,18 +13,20 @@ set_clock_uncertainty $clock_uncertainty [get_clocks core_clk]
 set_clock_transition $clock_transition [get_clocks core_clk]
 set_clock_latency $clock_latency [get_clocks core_clk]
 
-set_load 0.3 [all_outputs]
+#reduced the load capacitance value, giving us faster cells
+set_load 0.10 [all_outputs]
 set_driving_cell -no_design_rule -lib_cell NBUFFX4_RVT [all_inputs]
 
 # Need to add own constraints for each top module
 # Need to test with these added values now
-set_input_delay -max [expr $clock_period * 0.1] [get_ports -filter "direction == in" a*] -clock core_clk
-set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" a*] -clock core_clk
-set_input_delay -max [expr $clock_period * 0.1] [get_ports -filter "direction == in" b*] -clock core_clk
-set_input_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == in" b*] -clock core_clk
-set_output_delay -max [expr $clock_period * 0.1] [get_ports -filter "direction == out" c*] -clock core_clk
-set_output_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == out" c*] -clock core_clk
-set_output_delay -max [expr $clock_period * 0.1] [get_ports -filter "direction == out" mo*] -clock core_clk
-set_output_delay -min [expr $clock_period * 0.1] [get_ports -filter "direction == out" mo*] -clock core_clk
+# 0.05 specifies 5 percent of clock period and so on
+set_input_delay -max [expr $clock_period * 0.020] [get_ports -filter "direction == in" a*] -clock core_clk 
+set_input_delay -min [expr $clock_period * 0.010] [get_ports -filter "direction == in" a*] -clock core_clk
+set_input_delay -max [expr $clock_period * 0.020] [get_ports -filter "direction == in" b*] -clock core_clk
+set_input_delay -min [expr $clock_period * 0.010] [get_ports -filter "direction == in" b*] -clock core_clk
+set_output_delay -max [expr $clock_period * 0.030] [get_ports -filter "direction == out" c*] -clock core_clk
+set_output_delay -min [expr $clock_period * 0.020] [get_ports -filter "direction == out" c*] -clock core_clk
+set_output_delay -max [expr $clock_period * 0.025] [get_ports -filter "direction == out" mo*] -clock core_clk
+set_output_delay -min [expr $clock_period * 0.015] [get_ports -filter "direction == out" mo*] -clock core_clk
 
 set_false_path -from [get_ports -filter "direction == in" reset]
