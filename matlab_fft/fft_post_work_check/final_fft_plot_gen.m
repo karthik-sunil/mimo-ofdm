064bf700dfcb0607352908302da3d8a657c7b4b0
Hithesh Prabhakar Reddy <hitheshp@caen-vnc-mi14.engin.umich.edu>
2024-12-06 22:41:03 -0500

    Final set of matlab codes pushed

--- matlab_fft/fft_post_work_check/final_fft_plot_gen.m
+++ matlab_fft/fft_post_work_check/final_fft_plot_gen.m
@@ -1,6 +1,15 @@
 % params
 scaling_factor = 2^8; % scaling
-filename = 'a_outputsine.txt';
+% filename = 'a_fft64_verilogout.txt';
+% filename = 'a_fft64outputsine16.txt';
+% filename = 'a_fft8outputsine64.txt'; % 8 point
+% filename = 'a_fft_16outputsine.txt'; % 16 point
+% filename = 'a_fft32outputsine.txt'; % 32 point
+% filename = 'a_fft64outputsine.txt'; % 64 point
+% filename = 'a_fft128outputsine.txt'; % 128 point
+
+filename = 'check128ptfftout.txt';
+
 
 data = readmatrix(filename); %readinf data
 if size(data, 2) ~= 2
@@ -18,15 +27,20 @@ fft_magnitude = sqrt(real_float.^2 + imag_float.^2);
 
 N = length(fft_magnitude); % Number of FFT points
 Fs = 1000; % Sampling frequency (arbitrary)
-frequencies = (0:N-1) * (Fs / N)*(2); % Frequency axis in Hz
-% frequencies = (0:N-1) * (Fs / N);
+%frequencies = (0:N-1) * (Fs / N)*(2); % Frequency axis in Hz
+frequencies = (0:N-1) * (Fs / N);
 % fft_magnitude_half = fft_magnitude(1:N/2);
 
 % plot of FFT magnitude
 figure;
 stem(frequencies, fft_magnitude, 'r', 'LineWidth', 1.5);
+%stem(frequencies, suppressed_magnitude, 'b', 'LineWidth', 1.5); % Suppressed plot
+
 grid on;
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
-title('FFT Magnitude from fft\_N\_rad2.sv');
+% title('FFT Magnitude from Matlab Gen 128 point FFT');
+title('FFT Magnitude from our FFT engine for 128 point FFT');
+% title('FFT Magnitude from fft\_128\_rad2.sv');
+%title('FFT Magnitude supressed from fft\_64\_rad2.sv');
 xlim([0 1000]);
