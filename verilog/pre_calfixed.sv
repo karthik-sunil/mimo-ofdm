95938a94b961c9363845da68079bb00c8e642abf
Colormore <Colormore@pku.edu.cn>
2024-12-06 20:08:18 -0500

    pre

--- verilog/pre_calfixed.sv
+++ verilog/pre_calfixed.sv
@@ -1,5 +1,5 @@
-`include "matmul.sv"
-module pre_cal(
+
+module pre_calfixed(
     input logic clk,
     input logic reset,
     input logic [31:0] H_matrix[0:3][0:3],
@@ -10,6 +10,10 @@ module pre_cal(
     output logic [31:0] vector_b [0:3]
     
 );
+logic [31:0] input_H [0:3][0:3];
+logic [31:0] input_sig [0:3];
+logic [31:0] input_snr ;
+
 
 logic [31:0] H_hermitian [0:3][0:3];
 logic [31:0] A_add [0:3][0:3];
@@ -21,16 +25,16 @@ logic [31:0] matrix_b [0:3][0:3];
 always_comb begin
     for(int i=0; i<4;i++)begin
         for(int j=0;j<4;j++)begin
-            H_hermitian[i][j]=H_matrix[j][i];
+            H_hermitian[i][j]=input_H[j][i];
         end
     end
 end
 // get H^H
 
-matmul m1(
+matmulfixed m1(
 
     .a(H_hermitian),
-    .b(H_matrix),
+    .b(input_H),
     .out(A_mul)
 );
 always_comb begin
@@ -38,7 +42,7 @@ always_comb begin
         for(int j=0;j<4;j++)begin
 
             if(i==j) begin
-                A_add[i][j]=snr;
+                A_add[i][j]=input_snr;
             end else begin
                 A_add[i][j]=0;
             end
@@ -53,7 +57,7 @@ always_comb begin
     for (int i=0;i<4;i++)begin
         for(int j=0;j<4;j++)begin
             if(i==0)begin
-                matrix_sig[i][j]=signal_receive[j];
+                matrix_sig[i][j]=input_sig[j];
             end else begin
                 matrix_sig[i][j]=0;
             end
@@ -61,7 +65,7 @@ always_comb begin
     end
 end
 
-matmul m2(
+matmulfixed m2(
 
     .a(H_hermitian),
     .b(matrix_sig),
@@ -75,6 +79,17 @@ always_comb begin
 end
 // get vector B= H^H * r
 
+always_ff @(posedge clk) begin
+    if(reset)begin
+        input_snr<=0;
+        input_sig<=signal_receive;
+        input_H<=H_matrix;
+    end else begin
+        input_H<=H_matrix;
+        input_sig<=signal_receive;
+        input_snr<=snr;
+    end
+end
 
 
 
